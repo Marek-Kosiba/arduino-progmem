db3da2bca01a67f91def8aed5bd548af8c810126
Marek-Kosiba <106978319+Marek-Kosiba@users.noreply.github.com>
2022-06-06 18:25:02 +0200

    Add files via upload

---
+++ progmem.ino
@@ -0,0 +1,43 @@
+const PROGMEM  uint16_t charSet[]  = { 
+  65000, 32796, 
+  16843, 10, 
+  11234
+  };
+
+// save some chars
+const char signMessage[] PROGMEM  = {"I AM PREDATOR,  UNSEEN COMBATANT. CREATED BY THE UNITED STATES DEPART"};
+
+unsigned int displayInt;
+int k;    // counter variable
+char myChar;
+
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);  // wait for serial port to connect. Needed for native USB
+
+  // put your setup code here, to run once:
+  // read back a 2-byte int
+  for (k = 0; k < 5; k++)
+  {
+    displayInt = pgm_read_word_near(charSet + k);
+    Serial.println(displayInt);
+  }
+  Serial.println();
+__asm__("nop\n\t");
+
+__asm__("nop\n\t""nop\n\t""nop\n\t""nop\n\t"); 
+  // read back a char
+  for (k = 0; k < strlen_P(signMessage); k++)
+  {
+    myChar =  pgm_read_byte_near(signMessage + k);
+    Serial.print(myChar);
+  }
+
+  Serial.println();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
